2a1da62e3ac924da7eb2453c6a2e186ffd053ee0
wpsilva <wederpacheco.s@gmail.com>
2018-02-09 23:41:51 -0200

    remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
