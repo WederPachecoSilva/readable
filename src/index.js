cceff5aec47164233dd97fab7f87150d6dd48607
wpsilva <wederpacheco.s@gmail.com>
2018-02-10 23:32:17 -0200

    ts config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import App from './App';
+import App from './prod/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
