0062cd1facd995918a4a0dc647a0ea0ad90fa1f0
wpsilva <wederpacheco.s@gmail.com>
2018-02-21 18:52:17 -0300

    Throttle subscription to save state to local storage to improve performance

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
 
 import index from '../reducers/index';
 import { loadState, saveState } from './localStorage';
@@ -9,6 +10,6 @@ const cachedState = loadState();
 const store = createStore(index, cachedState, applyMiddleware(ReduxThunk));
 
 // Needs to change later to be called only when the desired cache state updates
-store.subscribe(() => saveState(store.getState()));
+store.subscribe(throttle(() => saveState(store.getState()), 1000));
 
 export default store;
