7ad498438b10ec78af9e01f17dfb11ef33e17607
wpsilva <wederpacheco.s@gmail.com>
2018-02-21 21:51:57 -0300

    Add flow type checking to posts related action creators

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,7 +1,41 @@
-import types from './types';
+// @flow
 
-const { FETCH_ALL_POSTS, FETCH_POSTS_BY_CATEGORY, FAILURE, FETCHING } = types;
+import { Dispatch } from 'redux';
+
+import {
+  FETCH_POSTS_BY_CATEGORY,
+  FETCH_ALL_POSTS,
+  FETCHING,
+  FAILURE,
+} from './types';
+import { getPosts, getPostsByCategory } from '../utils/readableAPI';
+
+export function fetchPostsByCategory(category: string) {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({ type: FETCHING });
+      const response = await getPostsByCategory(category);
+      dispatch({
+        type: FETCH_POSTS_BY_CATEGORY,
+        payload: response,
+      });
+    } catch (error) {
+      dispatch({ type: FAILURE });
+    }
+  };
+}
 
 export function fetchAllPosts() {
-  return dispatch => {};
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({ type: FETCHING });
+      const response = await getPosts();
+      dispatch({
+        type: FETCH_ALL_POSTS,
+        payload: response,
+      });
+    } catch (error) {
+      dispatch({ type: FAILURE });
+    }
+  };
 }
