e4b3ca701b2f1a932a511a16298928e00a054838
wpsilva <wederpacheco.s@gmail.com>
2018-02-21 21:49:26 -0300

    Add flow type checking to categories related action creators

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,17 +1,24 @@
-import { FETCHING, FETCH_CATEGORIES, FAILURE } from './types';
+// @flow
+
+import {
+  FETCH_CATEGORIES,
+  FETCH_CATEGORIES_SUCCESS,
+  FETCH_CATEGORIES_FAILURE,
+} from './types';
+import { Dispatch } from 'redux';
 import * as API from '../utils/readableAPI';
 
 export const fetchCategories = () => {
-  return async dispatch => {
-    dispatch({ type: FETCHING });
+  return async (dispatch: Dispatch) => {
+    dispatch({ type: FETCH_CATEGORIES });
     try {
       const response = await API.getCategories();
       dispatch({
-        type: FETCH_CATEGORIES,
+        type: FETCH_CATEGORIES_SUCCESS,
         payload: response,
       });
     } catch (error) {
-      dispatch({ type: FAILURE, payload: error });
+      dispatch({ type: FETCH_CATEGORIES_FAILURE, payload: error });
     }
   };
 };
