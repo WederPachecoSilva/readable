62d171aafb31fa5796a083a7b2dcdbb44dccd217
wpsilva <wederpacheco.s@gmail.com>
2018-02-22 09:47:03 -0300

    Add comments action creators

---
+++ src/actions/comments.js
@@ -0,0 +1,28 @@
+// @flow
+import { Dispatch } from 'redux';
+
+import { getCommentsByPost } from '../utils/readableAPI';
+import {
+  FETCH_COMMENTS_BY_POST,
+  FETCH_COMMENTS_BY_POST_SUCCESS,
+  FETCH_COMMENTS_BY_POST_FAILURE,
+} from './types';
+
+export function fetchCommentsByPost(postId: string) {
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch({ type: FETCH_COMMENTS_BY_POST });
+      const response = await getCommentsByPost(postId);
+      response;
+      dispatch({
+        type: FETCH_COMMENTS_BY_POST_SUCCESS,
+        payload: response,
+      });
+    } catch (error) {
+      dispatch({
+        type: FETCH_COMMENTS_BY_POST_FAILURE,
+        payload: error,
+      });
+    }
+  };
+}
