0a94599de4a9f2a9f25e85a953c51bd94e95bd25
wpsilva <wederpacheco.s@gmail.com>
2018-03-03 18:55:09 -0300

    Fix typo

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,8 +1,6 @@
 // @flow
 
-import {
-  Action
-} from 'redux';
+import { Action } from 'redux';
 
 import {
   FETCH_ALL_POSTS_SUCCESS,
@@ -12,27 +10,18 @@ import {
   VOTE_POST_SUCCESS,
   UPDATE_POST_SUCCESS,
 } from '../actions/types';
-import type {
-  Posts
-} from '../utils/flowTypes';
-
-const deleteProperty = (object: any, property: string) =>
-  Object.keys(object).reduce((acc, key) => key !== property ? { ...acc,
-    [key]: object[key]
-  } : acc, {})
+import type { Posts } from '../utils/flowTypes';
+import { deleteProperty } from '../utils/helpers';
 
 export default function posts(state: Posts | {} = {}, action: Action) {
-  const {
-    payload,
-    type
-  } = action;
+  const { payload, type } = action;
 
   switch (type) {
     case FETCH_ALL_POSTS_SUCCESS:
       return payload;
 
     case DELETE_POST_SUCCESS:
-      return deleteProperty(state, payload.id)
+      return deleteProperty(state, payload.id);
 
     case ADD_POST_SUCCESS:
     case VOTE_POST_SUCCESS:
