7efabf203cb697266a60a83884b68fb9ff69e66c
wpsilva <wederpacheco.s@gmail.com>
2018-03-05 23:32:52 -0300

    Add failures reducer to store config

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,8 @@ import { combineReducers } from 'redux';
 import categories from './categories';
 import posts from './posts';
 import comments from './comments';
+import failures from './failures';
 
-const index = combineReducers({ categories, posts, comments });
+const index = combineReducers({ categories, posts, comments, failures });
 
 export default index;
