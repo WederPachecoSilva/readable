ce692f6c6ed57136edc662da22c9264990d30da7
wpsilva <wederpacheco.s@gmail.com>
2018-03-13 00:23:37 -0300

    Bug fix in server

--- api-server/server.js
+++ api-server/server.js
@@ -13,6 +13,7 @@ const comments = require('./comments');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -206,7 +207,7 @@ app.get('/posts', (req, res) => {
   );
 });
 
-app.post('/posts', bodyParser.json(), (req, res) => {
+app.post('/posts', (req, res) => {
   posts.add(req.token, req.body).then(
     data => res.send(data),
     error => {
@@ -245,7 +246,7 @@ app.delete('/posts/:id', (req, res) => {
     );
 });
 
-app.post('/posts/:id', bodyParser.json(), (req, res) => {
+app.post('/posts/:id', (req, res) => {
   const { option } = req.body;
   const id = req.params.id;
   posts.vote(req.token, id, option).then(
@@ -259,7 +260,7 @@ app.post('/posts/:id', bodyParser.json(), (req, res) => {
   );
 });
 
-app.put('/posts/:id', bodyParser.json(), (req, res) => {
+app.put('/posts/:id', (req, res) => {
   posts.edit(req.token, req.params.id, req.body).then(
     data => res.send(data),
     error => {
@@ -295,7 +296,7 @@ app.get('/comments/:id', (req, res) => {
   );
 });
 
-app.put('/comments/:id', bodyParser.json(), (req, res) => {
+app.put('/comments/:id', (req, res) => {
   comments.edit(req.token, req.params.id, req.body).then(
     data => res.send(data),
     error => {
@@ -307,7 +308,7 @@ app.put('/comments/:id', bodyParser.json(), (req, res) => {
   );
 });
 
-app.post('/comments', bodyParser.json(), (req, res) => {
+app.post('/comments', (req, res) => {
   comments.add(req.token, req.body).then(
     data => res.send(data),
     error => {
@@ -319,7 +320,7 @@ app.post('/comments', bodyParser.json(), (req, res) => {
   );
 });
 
-app.post('/comments/:id', bodyParser.json(), (req, res) => {
+app.post('/comments/:id', (req, res) => {
   const { option } = req.body;
   comments.vote(req.token, req.params.id, option).then(
     data => res.send(data),
