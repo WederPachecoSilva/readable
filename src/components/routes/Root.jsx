6dab6d735d7ff5a819e84ebd358a2c36eed09c98
Wéder Pacheco <wederpachecosilva@gmail.com>
2018-03-26 17:31:45 -0300

    Finish Root route component

--- src/components/routes/Root.jsx
+++ src/components/routes/Root.jsx
@@ -2,27 +2,37 @@
 
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Grid } from 'material-ui';
+import Grid from 'material-ui/Grid';
 
-import PostCard from '../PostCard';
+import { fetchAllPosts } from '../../actions/posts';
+import PostCard from '../PostCard/PostCard';
 import withErrorBoundary from '../errorHandling/withErrorBoundary';
 
-const Root = ({ posts, dispatch }) => {
-  const ids = Object.keys(posts);
-  return (
-    <React.Fragment>
-      {ids.map(id => (
-        <Grid container justify="center" key={id}>
-          <PostCard
-            style={{ margin: '2em' }}
-            post={posts[id]}
-            dispatch={dispatch}
-          />
-        </Grid>
-      ))}
-    </React.Fragment>
-  );
-};
+class Root extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchAllPosts());
+  }
+
+  render() {
+    const { posts, dispatch } = this.props;
+    const ids = Object.keys(posts);
+    return (
+      <React.Fragment>
+        {ids.map(id => {
+          return posts[id].deleted === false ? (
+            <Grid container justify="center" key={id}>
+              <PostCard
+                style={{ margin: '2em' }}
+                post={posts[id]}
+                dispatch={dispatch}
+              />
+            </Grid>
+          ) : null;
+        })}
+      </React.Fragment>
+    );
+  }
+}
 
 const mapState = state => ({ posts: state.posts });
 
