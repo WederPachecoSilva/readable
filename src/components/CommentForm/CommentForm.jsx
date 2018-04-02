fbc3ccf9deb1c50469d9d572874a54adbfe029fe
wpsilva <wederpacheco.s@gmail.com>
2018-04-02 19:11:15 -0300

    Add CommentForm component

---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,85 @@
+// @ts-check
+
+import React from 'react';
+import v4 from 'uuid/v4';
+import { connect } from 'react-redux';
+import { Grid, withStyles } from 'material-ui';
+
+import Alert from '../primitives/Alert';
+import Input from '../primitives/Input';
+import TextArea from '../primitives/TextArea';
+import { addComment } from '../../actions/comments';
+import { fetchPost } from '../../actions/posts';
+
+class CommentForm extends React.Component {
+  state = { author: '', body: '', error: false };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { author, body } = this.state;
+    if (!author || !body) {
+      this.setState({ error: true });
+      return;
+    }
+    const id = v4();
+    const timestamp = new Date();
+    const { post, dispatch } = this.props;
+    dispatch(addComment({ id, author, body, timestamp, parentId: post.id }));
+    this.setState({ author: '', body: '' });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid className={classes.container} item md={8} sm={10} xs={12}>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="author"
+            handleChange={this.handleChange}
+            value={this.state.author}
+            label="Author"
+          />
+          <br />
+          <TextArea
+            name="body"
+            rows={4}
+            value={this.state.body}
+            handleChange={this.handleChange}
+            label="Comment"
+          />
+          <Grid container justify="center">
+            <button className={classes.button} type="submit">
+              Submit
+            </button>
+          </Grid>
+        </form>
+      </Grid>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    borderRadius: '5px',
+    backgroundColor: '#F7F7F7',
+    padding: '5em',
+    margin: '2em',
+  },
+  button: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '1em',
+    width: '10em',
+    height: '2.3em',
+    backgroundColor: '#28739E',
+    color: 'black',
+    border: '1px #28739E solid',
+    borderRadius: '10px',
+  },
+};
+
+export default withStyles(styles)(connect()(CommentForm));
