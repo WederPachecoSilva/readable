fde72499a1c6f408ef3a1e0725babd1cedd90035
wpsilva <wederpacheco.s@gmail.com>
2018-04-03 09:35:46 -0300

    Add sortDate method to helpers file

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,11 @@
-// @flow
-
 /**
  *  Pure function that returns a new object without the property passed as
  *  second parameter.
+ * @param {any} object
+ * @param {string} property
+ * @returns {any}
  */
-export const deleteProperty = (object: any, property: string) =>
+export const deleteProperty = (object, property) =>
   Object.keys(object).reduce(
     (acc, key) =>
       key !== property
@@ -15,3 +16,22 @@ export const deleteProperty = (object: any, property: string) =>
         : acc,
     {}
   );
+
+/**
+ * Sort an array of dates
+ * @param {'ascendet' | 'descendent'} type
+ * @param {string[]} dates
+ * @returns {string[]}
+ */
+export const sortDate = (type, dates) => {
+  switch (type) {
+    case 'ascendent':
+      return dates.sort((date1, date2) => (date1 > date2 ? 1 : -1));
+
+    case 'descendent':
+      return dates.sort((date1, date2) => (date1 > date2 ? -1 : 1));
+
+    default:
+      return;
+  }
+};
