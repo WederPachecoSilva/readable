b499e77ad30d66a80c458a8760d9b0b39ecc41be
wpsilva <wederpacheco.s@gmail.com>
2018-04-02 23:47:18 -0300

    Change lib from reactstrap to material-ui in Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,74 +1,37 @@
 // @ts-check
 
 import React from 'react';
-import {
-  NavbarToggler,
-  Navbar,
-  NavbarBrand,
-  NavItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  Collapse,
-  Nav,
-  DropdownItem,
-} from 'reactstrap';
+import { AppBar, Toolbar, Typography } from 'material-ui';
 import { Link, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import { fetchCategories } from '../../actions/categories';
+import HeaderMenu from './HeaderMenu';
 
-class Header extends React.Component {
-  state = {
-    isOpen: false,
-  };
+const Header = ({ location, categories }) => {
+  const { pathname } = location;
+  return (
+    <AppBar position="static" style={styles.container}>
+      <Toolbar>
+        <Typography variant="title" color="inherit" style={styles.brand}>
+          Title
+        </Typography>
+        <div style={styles.links}>
+          {pathname !== '/' && (
+            <Link style={styles.link} to="/">
+              <p>Home</p>
+            </Link>
+          )}
+          {pathname !== '/add' && (
+            <Link style={styles.link} to="/add">
+              <p>Add Post</p>
+            </Link>
+          )}
 
-  componentDidMount() {
-    this.props.dispatch(fetchCategories());
-  }
-
-  toggleNav = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-
-  render() {
-    const { pathname } = this.props.location;
-    const { categories } = this.props;
-    const catIds = Object.keys(categories);
-    return (
-      <Navbar light expand="md" style={styles.container}>
-        <NavbarBrand style={styles.brand}>Readable</NavbarBrand>
-        <NavbarToggler onClick={this.toggleNav} />
-        <Collapse navbar isOpen={this.state.isOpen}>
-          <Nav style={styles.nav} className="ml-auto" navbar>
-            {pathname !== '/' && (
-              <Link style={styles.link} to="/">
-                <NavItem>Home</NavItem>
-              </Link>
-            )}
-            {pathname !== '/add' && (
-              <Link style={styles.link} to="/add">
-                <NavItem>Add Post</NavItem>
-              </Link>
-            )}
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Subject
-              </DropdownToggle>
-              <DropdownMenu>
-                {catIds.map(id => (
-                  <Link key={id} to={`/posts/${categories[id].path}`}>
-                    <DropdownItem>{categories[id].name}</DropdownItem>
-                  </Link>
-                ))}
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
-}
+          <HeaderMenu />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 const styles = {
   container: {
@@ -80,17 +43,16 @@ const styles = {
     marginLeft: '4em',
     color: 'grey',
   },
-  nav: {
-    marginRight: '5em',
-  },
   link: {
     color: 'grey',
     margin: 'auto 1em',
   },
+  links: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 'auto',
+    marginRight: '1em',
+  },
 };
 
-const mapState = ({ categories }) => ({
-  categories,
-});
-
-export default withRouter(connect(mapState)(Header));
+export default withRouter(Header);
