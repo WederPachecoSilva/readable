3c0b1797277070ce5b8ef670abc75d0896f061de
wpsilva <wederpacheco.s@gmail.com>
2018-04-05 14:49:38 -0300

    Finish PostsByCategory logic and add prop-types checking to it

--- src/components/routes/PostsByCategory.jsx
+++ src/components/routes/PostsByCategory.jsx
@@ -1,7 +1,39 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Grid } from 'material-ui';
+import PropTypes from 'prop-types';
 
+import { fetchAllPosts } from '../../actions/posts';
+import PostCard from '../PostCard/PostCard';
 import withErrorBoundary from '../errorHandling/withErrorBoundary';
 
-const PostsByCategory = () => <p>posts by category</p>;
+class PostsByCategory extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchAllPosts());
+  }
 
-export default withErrorBoundary(PostsByCategory);
+  render() {
+    const { posts } = this.props;
+    const ids = Object.keys(posts);
+
+    return (
+      <Grid container justify="center" alignItems="center" spacing={16}>
+        {ids.map(
+          id =>
+            posts[id].category === this.props.match.params.category && (
+              <PostCard key={id} post={posts[id]} />
+            )
+        )}
+      </Grid>
+    );
+  }
+}
+
+const mapState = ({ posts }) => ({ posts });
+
+PostsByCategory.propTypes = {
+  dispatch: PropTypes.func,
+  posts: PropTypes.object,
+};
+
+export default withErrorBoundary(connect(mapState)(PostsByCategory));
