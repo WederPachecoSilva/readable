6bea6329865c7c73e73798ab4433f92bf04108b0
wpsilva <wederpacheco.s@gmail.com>
2018-04-05 14:55:32 -0300

    Finish PostDetail logic and add prop-types checking to it

--- src/components/routes/PostDetail.jsx
+++ src/components/routes/PostDetail.jsx
@@ -1,7 +1,46 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Grid } from 'material-ui';
+import PropTypes from 'prop-types';
 
+import PostCard from '../PostCard/PostCard';
+import CommentCard from '../CommentCard/CommentCard';
+import { fetchCommentsByPost } from '../../actions/comments';
+import { fetchPost } from '../../actions/posts';
 import withErrorBoundary from '../errorHandling/withErrorBoundary';
+import CommentForm from '../CommentForm/CommentForm';
 
-const PostDetail = () => <p>post detail</p>;
+class PostDetail extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPost(this.props.match.params.id));
+    this.props.dispatch(fetchCommentsByPost(this.props.match.params.id));
+  }
 
-export default withErrorBoundary(PostDetail);
+  render() {
+    const { post, comments } = this.props;
+    const commentsIds = Object.keys(comments);
+    return (
+      <Grid container justify="center" alignItems="center" spacing={16}>
+        {post && <PostCard post={post} />}
+        {commentsIds.map(id => <CommentCard key={id} comment={comments[id]} />)}
+        {post && <CommentForm post={post} />}
+      </Grid>
+    );
+  }
+}
+
+const mapState = (state, ownProps) => {
+  const postId = ownProps.match.params.id;
+  return {
+    post: state.posts[postId],
+    comments: state.comments,
+  };
+};
+
+PostDetail.propTypes = {
+  dispatch: PropTypes.func,
+  post: PropTypes.object,
+  comments: PropTypes.object,
+};
+
+export default connect(mapState)(withErrorBoundary(PostDetail));
