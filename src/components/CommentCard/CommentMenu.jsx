932b8f13eeb0d6aaf890ba8f3bd9b63e0f2a922f
wpsilva <wederpacheco.s@gmail.com>
2018-04-05 23:11:14 -0300

    Create CommentMenu component

---
+++ src/components/CommentCard/CommentMenu.jsx
@@ -0,0 +1,45 @@
+// @ts-check
+
+import React from 'react';
+import { Menu, MenuItem, withStyles, Popover } from 'material-ui';
+import { NavLink } from 'react-router-dom';
+import MoreVertIcon from 'material-ui-icons/MoreVert';
+import PropTypes from 'prop-types';
+
+const CommentMenu = ({
+  anchorEl,
+  editCommentLink,
+  deleteComment,
+  closeMenu,
+  openMenu,
+  classes,
+}) => (
+  <React.Fragment>
+    <MoreVertIcon onClick={openMenu} />
+
+    <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={closeMenu}>
+      <NavLink className={classes.editLink} to={editCommentLink}>
+        <MenuItem>Edit Comment</MenuItem>
+      </NavLink>
+      <MenuItem onClick={deleteComment}>Delete Comment</MenuItem>
+    </Menu>
+  </React.Fragment>
+);
+
+const styles = {
+  editLink: {
+    color: 'black',
+    textDecoration: 'none',
+  },
+};
+
+CommentMenu.propTypes = {
+  anchorEl: PropTypes.object.isRequired,
+  editCommentLink: PropTypes.string.isRequired,
+  deleteComment: PropTypes.func.isRequired,
+  closeMenu: PropTypes.func.isRequired,
+  openMenu: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CommentMenu);
